cbf768f0b8de3376278a467091d32a01911b67e4
Samuel Reed <samuel.trace.reed@gmail.com>
2014-07-10 14:08:21 -0500

    Remove peerDependencies, use express directly.

--- index.js
+++ index.js
@@ -5,11 +5,8 @@
 var path = require('path');
 var urlJoin = require('./lib/url-join');
 var _defaults = require('lodash.defaults');
-var extend = require('util')._extend;
-var loopback = require('loopback');
-var express = requireLoopbackDependency('express');
+var express = require('express');
 var swagger = require('./lib/swagger');
-var fs = require('fs');
 var SWAGGER_UI_ROOT = path.join(__dirname, 'node_modules', 
   'swagger-ui', 'dist');
 var STATIC_ROOT = path.join(__dirname, 'public');
@@ -58,33 +55,12 @@ function explorer(loopbackApplication, options) {
   // In this way one could e.g. make changes to index.html without having 
   // to worry about constantly pulling in JS updates.
   if (options.swaggerDistRoot) {
-    app.use(loopback.static(options.swaggerDistRoot));
+    app.use(express.static(options.swaggerDistRoot));
   }
   // File in node_modules are overridden by a few customizations
-  app.use(loopback.static(STATIC_ROOT));
+  app.use(express.static(STATIC_ROOT));
   // Swagger UI distribution
-  app.use(loopback.static(SWAGGER_UI_ROOT));
+  app.use(express.static(SWAGGER_UI_ROOT));
 
   return app;
-}
-
-function requireLoopbackDependency(module) {
-  try {
-    return require('loopback/node_modules/' + module);
-  } catch (err) {
-    if (err.code !== 'MODULE_NOT_FOUND') throw err;
-    try {
-      // Dependencies may be installed outside the loopback module,
-      // e.g. as peer dependencies. Try to load the dependency from there.
-      return require(module);
-    } catch (errPeer) {
-      if (errPeer.code !== 'MODULE_NOT_FOUND') throw errPeer;
-      // Rethrow the initial error to make it clear that we were trying
-      // to load a module that should have been installed inside
-      // "loopback/node_modules". This should minimise end-user's confusion.
-      // However, such situation should never happen as `require('loopback')`
-      // would have failed before this function was even called.
-      throw err;
-    }
-  }
 }
