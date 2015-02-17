16c54c038e60e7ef05d6f9252ad1883d475350b5
gandrianakis <g.andrianakis@internetq.com>
2015-02-17 17:23:32 +0200

    Made API doc of class use the http.path of the class if available, or the name of the class as a fallback

--- test/class-helper.test.js
+++ test/class-helper.test.js
@@ -12,12 +12,32 @@ describe('class-helper', function() {
 
     expect(doc.description).to.equal('line1\nline2');
   });
+
+  it('sets resourcePath from aClass.http.path', function() {
+    var doc = generateAPIDoc({}, 'otherPath');
+
+    expect(doc.resourcePath).to.equal('/otherPath');
+  });
+
+  it('sets resourcePath from aClass.name', function() {
+    var doc = generateAPIDoc({});
+
+    expect(doc.resourcePath).to.equal('/test');
+  });
 });
 
 // Easy wrapper around createRoute
 function generateResourceDocAPIEntry(def) {
-  return classHelper.generateResourceDocAPIEntry(_defaults(def, {
-    http: { path: '/test' },
-    ctor: { settings: { } },
-  }));
+    return classHelper.generateResourceDocAPIEntry(_defaults(def, {
+        http: { path: '/test' },
+        ctor: { settings: { } }
+    }));
+}
+
+function generateAPIDoc(def, httpPath) {
+    return classHelper.generateAPIDoc(_defaults(def, {
+        http: { path: httpPath || null },
+        name: 'test',
+        ctor: { settings: { } }
+    }), {resourcePath: 'resources'});
 }
