3bb40e3c598bae6093753cb15598417203d1e57e
Samuel Reed <samuel.trace.reed@gmail.com>
2017-09-11 08:26:05 -0500

    feat(example): Expand simple example

--- example/simple.js
+++ example/simple.js
@@ -16,6 +16,43 @@ var Product = loopback.PersistedModel.extend('product', {
   aNum: { type: 'number', min: 1, max: 10, required: true, default: 5 },
 });
 Product.attachTo(loopback.memory());
+
+// Some Swagger options - these can be specified in Model JSON:
+Product.definition.settings.swagger = {
+  tag: {
+    name: 'Product Tag Name',
+    externalDocs: {
+      description: 'More Docs',
+      url: 'http://my-external-spec.example.com',
+    },
+  },
+};
+Product.definition.settings.description = 'Products for People';
+
+// A custom remoteMethod
+Product.foo = function() { return Promise.resolve(); };
+/*eslint-disable*/
+Product.remoteMethod(
+  'foo',
+  {
+    description: 'A Foo Product Transmogrifier.',
+    notes: 'Ethical poutine raw denim, everyday carry twee truffaut kale chips schlitz. ' +
+      'Actually microdosing jean shorts',
+    accepts: [
+      {arg: 'name', type: 'string', required: true, description: 'Product name.'},
+      {arg: 'description', type: 'string', required: true, description: 'Product description.'},
+      {arg: 'tag', type: 'string', required: false, description: 'Product tag.'},
+      {arg: 'count', type: 'number', description: 'How many products to transmogrify.'},
+      {arg: 'locale', type: 'string', description: 'Your locale.', default: 'en-US'},
+    ],
+    returns: {
+      arg: 'product', type: 'product', root: true, description: 'Foo Product.'
+    },
+    http: {verb: 'post', path: '/foo'},
+  }
+);
+/*eslint-enable*/
+
 app.model(Product);
 
 var apiPath = '/api';
